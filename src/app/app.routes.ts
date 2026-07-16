55eec2326ed7c7d125f84955944e41db89b7bdf3
Naor <barazaninaor@gmail.com>
2026-07-16 12:36:19 +0300

    Update routing to use HashLocationStrategy for GitHub Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import { TransactionsComponent } from './transactions/transactions.component';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router'; // הוסף את withHashLocation
 
 export const routes: Routes = [
   { path: 'transactions', component: TransactionsComponent },
   { path: '', redirectTo: '/transactions', pathMatch: 'full' } // Redirect to transactions by default
 ];
+
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withHashLocation()) // כאן הוספנו את ה-Hash
+  ]
+};
